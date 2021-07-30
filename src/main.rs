8ca47deac7c25d185f68e409e11a10b414d5951b
Kushal Das <mail@kushaldas.in>
2021-07-30 21:53:23 +0530

    Adds connect options
    We can now connect to a given host and port combination.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{Error, Read, Write};
-use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
+use std::io::{self, Error, Read, Write};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream, ToSocketAddrs};
 extern crate clap;
 use clap::{App, Arg};
 
@@ -16,10 +16,24 @@ fn main() -> Result<(), Error> {
                 .help("Listens to the given port")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("domain")
+                .help("Domain name or IP address to connect to.")
+                .index(1)
+                .requires("port"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .help("The port number to connect to.")
+                .requires("domain")
+                .index(2),
+        )
         .get_matches();
     // let us see the arugments
     let listen = matches.value_of("listen").unwrap_or("0");
 
+    // buffer
+    let mut data = [0_u8; 1024];
     // If we have to listen
     if listen != "0" {
         // Let us listen to the given port
@@ -29,8 +43,6 @@ fn main() -> Result<(), Error> {
         let listener = TcpListener::bind(socket)?;
         let (mut stream, _addr) = listener.accept()?;
 
-        // buffer
-        let mut data = [0_u8; 1024];
         // We need stdout as we may print direct BINARY data
         let mut out = std::io::stdout();
         loop {
@@ -43,7 +55,52 @@ fn main() -> Result<(), Error> {
         }
         // All done, now return
         return Ok(());
-
     }
+    // We will try to connect to the given domain/ip.
+    let domain = match matches.value_of("domain") {
+        Some(name) => name,
+        None => {
+            eprintln!("Provide a domain name and port number to connect to.");
+            return Ok(());
+        }
+    };
+
+    let port = match matches.value_of("port") {
+        Some(name) => name,
+        None => {
+            eprintln!("Provide a port number to connect to.");
+            return Ok(());
+        }
+    };
+
+    // We need this to find the correct IP address
+    let connection_string = format!("{}:{}", domain, port);
+
+    // TODO: Select v4 or v6 based on user input
+    let mut ip_addrs = match connection_string.to_socket_addrs() {
+        Ok(ips) => ips,
+        Err(_) => {
+            eprintln!("Error parsing domain/IP:port combination.");
+            return Ok(());
+        }
+    };
+
+    // The ip address to connect to
+    let _socket = ip_addrs.next().unwrap();
+    dbg!(_socket);
+
+    let mut stream = TcpStream::connect(_socket)?;
+
+    loop {
+        match io::stdin().read(&mut data) {
+            Ok(bytes_read) => {
+                stream.write_all(&data[..bytes_read])?;
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+
     Ok(())
 }
