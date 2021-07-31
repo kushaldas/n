a8b8b1fe1cb261c9fe336a14046153ad919c9d3a
Kushal Das <mail@kushaldas.in>
2021-07-31 15:58:22 +0530

    This does not exist from the loop if a client disconnect

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::{self, Read, Write};
+use std::io::Write;
 use std::net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs};
-use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 extern crate clap;
 use clap::{App, Arg};
 
@@ -36,72 +36,81 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listen = matches.value_of("listen").unwrap_or("0");
 
     // buffer
-    let mut data = [0_u8; 1024];
+    let mut reader_buf = [0_u8; 1024];
+    let mut writer_buf = [0_u8; 1024];
     // If we have to listen
-    if listen != "0" {
+    let mut stream = if listen != "0" {
         // Let us listen to the given port
         //
         let ip = Ipv4Addr::new(0, 0, 0, 0);
         let socket = SocketAddrV4::new(ip, listen.parse::<u16>().unwrap());
         let listener = TcpListener::bind(socket).await?;
-        let (mut stream, _addr) = listener.accept().await?;
+        let (stream, _addr) = listener.accept().await?;
+        stream
 
-        // We need stdout as we may print direct BINARY data
-        let mut out = std::io::stdout();
-        loop {
-            let bytes_read = stream.read(&mut data).await?;
-            if bytes_read == 0 {
-                out.flush()?;
-                break;
+    } else {
+        // We will try to connect to the given domain/ip.
+        let domain = match matches.value_of("domain") {
+            Some(name) => name,
+            None => {
+                eprintln!("Provide a domain name and port number to connect to.");
+                return Ok(());
             }
-            out.write_all(&data[..bytes_read])?;
-            out.flush()?;
-        }
-        // All done, now return
-        return Ok(());
-    }
-    // We will try to connect to the given domain/ip.
-    let domain = match matches.value_of("domain") {
-        Some(name) => name,
-        None => {
-            eprintln!("Provide a domain name and port number to connect to.");
-            return Ok(());
-        }
-    };
+        };
 
-    let port = match matches.value_of("port") {
-        Some(name) => name,
-        None => {
-            eprintln!("Provide a port number to connect to.");
-            return Ok(());
-        }
-    };
-
-    // We need this to find the correct IP address
-    let connection_string = format!("{}:{}", domain, port);
-
-    // TODO: Select v4 or v6 based on user input
-    let mut ip_addrs = match connection_string.to_socket_addrs() {
-        Ok(ips) => ips,
-        Err(_) => {
-            eprintln!("Error parsing domain/IP:port combination.");
-            return Ok(());
-        }
-    };
-
-    // The ip address to connect to
-    let _socket = ip_addrs.next().unwrap();
-    dbg!(_socket);
-
-    let mut stream = TcpStream::connect(_socket).await?;
-
-    loop {
-        match io::stdin().read(&mut data) {
-            Ok(bytes_read) => {
-                stream.write_all(&data[..bytes_read]).await?;
+        let port = match matches.value_of("port") {
+            Some(name) => name,
+            None => {
+                eprintln!("Provide a port number to connect to.");
+                return Ok(());
             }
+        };
+
+        // We need this to find the correct IP address
+        let connection_string = format!("{}:{}", domain, port);
+
+        // TODO: Select v4 or v6 based on user input
+        let mut ip_addrs = match connection_string.to_socket_addrs() {
+            Ok(ips) => ips,
             Err(_) => {
-                break;
+                eprintln!("Error parsing domain/IP:port combination.");
+                return Ok(());
+            }
+        };
+
+        // The ip address to connect to
+        let _socket = ip_addrs.next().unwrap();
+        dbg!(_socket);
+
+        let stream = TcpStream::connect(_socket).await?;
+        stream
+    };
+
+    let (mut tx, mut rx) = stream.split();
+
+    let mut out = tokio::io::stdout();
+    let mut stdin = tokio::io::stdin();
+    loop {
+        tokio::select! {
+            bytes_read = tx.read(&mut reader_buf) =>{
+                let actual_read = bytes_read.unwrap();
+                if actual_read == 0 {
+                    // Means nothing to read
+                    // End of loop
+                    out.flush().await?;
+                    break;
+                } else {
+                    out.write_all(&reader_buf[..actual_read]).await?;
+                    out.flush().await?;
+                }
+            }
+            bytes_read = stdin.read(&mut writer_buf) => {
+                let actual_read = bytes_read.unwrap();
+                if actual_read == 0 {
+                    break;
+                } else {
+                    rx.write_all(&writer_buf[..actual_read]).await?;
+                }
             }
         }
     }
